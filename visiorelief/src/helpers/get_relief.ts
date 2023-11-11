a10cd6fa0bb354b3f686d9b71af5307a4b03c249
Malcolm Vassallo <malcolmvas@gmail.com>
2023-11-11 12:53:49 +0200

    Request to openai from button

--- visiorelief/src/helpers/get_relief.ts
+++ visiorelief/src/helpers/get_relief.ts
@@ -11,13 +11,39 @@ export default async () => {
     sounds.push(sound1);
     const chosen_sounds = [sound1, sound2];
 
-    //start playing the sounds as intro
-    console.log(scene);
-    console.log(chosen_sounds);
+    const url = "http://localhost:5000/query"
+
+    //start playing the sounds as intro.
+    var audio = new Audio(sound1+'.mp3');
+    audio.play();
+
+    console.log(scene)
+    console.log(chosen_sounds)
 
     //Send request to backend with the required variables
-    fetch('/backend');
-
+    fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json' // Specify the content type as JSON
+        },
+        body: JSON.stringify({scene: scene, sounds: chosen_sounds, length: 100}) // Convert the dataObject to a JSON string
+      })
+        .then(response => {
+          // Handle the response
+          if (response.ok) {
+            return response.json(); // Parse the JSON response if needed
+          } else {
+            throw new Error('Network response was not ok.');
+          }
+        })
+        .then(data => {
+          // Handle the data returned from the server
+          console.log(data);
+        })
+        .catch(error => {
+          // Handle errors
+          console.error('There was a problem with the fetch operation:', error);
+        });
     //wait 2 minutes to TTS the text
 
     //ask user for how they felt after the session
