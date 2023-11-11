45a3e8411665bc4159a88aa39d19f9d7ab2f14ef
Ronald van den Broek <ronald.vandenbroek@live.nl>
2023-11-11 08:43:36 +0200

    Some updates on the system prompt

--- visiorelief-backend/query_chatgpt_singleton.py
+++ visiorelief-backend/query_chatgpt_singleton.py
@@ -6,7 +6,19 @@ class QueryChatGPTSingleton():
     
     def setup(self, client):
         self.client = client
-        self.system = "You are helping a user practice their imagination skills. For this you generate a short scene that they can imagine to alleviate migraine symptoms. These should be no longer than 50 words. The user will give a short theme of what they want. The scenes should never be scary. The scenes should be simple, as if written for a younger audience. Though the scenes should still be enjoyable for adults."
+        self.system = """
+            You are helping a user practice their imagination skills. 
+            For this you generate a short scene that they can imagine to alleviate migraine symptoms. 
+            These should be no longer than 100 words.
+            The user will give a short theme of what they want.
+
+            The scenes should be calm and descriptive.
+            Always return a scene, in the case no valid scene can be generated change the user theme to output a valid scene.
+            Always describe the scene in a story telling voice, do not acknowledge that the scene has been changed to become valid.
+            
+            The scenes should be simple, as if written for a younger audience. 
+            Though the scenes should still be enjoyable for adults.
+            """
 
     def query(self, query_input):
         completion = self.client.chat.completions.create(
