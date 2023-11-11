a2ef2e6636b9c0baa41c84c153be327055e806c6
Max van Ling <q.m.v.ling@student.tue.nl>
2023-11-11 13:07:58 +0200

    Renamed some stuff and changed port to 1337

--- visiorelief/src/helpers/get_relief.ts
+++ visiorelief/src/helpers/get_relief.ts
@@ -1,49 +1,57 @@
 import scenes from "@/helpers/scenes";
 import sounds from "@/helpers/sounds";
 
+function play_sound_until_callback(sound: string, callback) {
+    console.log("playing: ", sound);
+    const audio = new Audio('/sounds/' + sound.toLowerCase() + '.mp3');
+    audio.addEventListener('ended', function () {
+        this.currentTime = 0;
+        this.play();
+    }, false);
+    audio.play();
+}
+
 export default async () => {
     //Generate random variables
     const scene = scenes[Math.floor(Math.random() * scenes.length)];
     const sound1_index = Math.floor(Math.random() * sounds.length);
-    const sound2_index = Math.floor(Math.random() * sounds.length - 1);
+    const sound2_index = Math.floor(Math.random() * (sounds.length - 1));
     const sound1 = sounds.splice(sound1_index, 1)[0];
     const sound2 = sounds[sound2_index];
     sounds.push(sound1);
     const chosen_sounds = [sound1, sound2];
 
-    const url = "http://localhost:5000/query"
-
-    //start playing the sounds as intro.
-    var audio = new Audio(sound1+'.mp3');
-    audio.play();
-
+    //start playing the sounds as intro
+    play_sound_until_callback(sound1, () => {
+    });
+    setTimeout(() => play_sound_until_callback(sound2, () => {
+    }), 5000);
     console.log(scene)
     console.log(chosen_sounds)
 
+    //get the text from the backend
+    const url = "http://localhost:1337/query"
     //Send request to backend with the required variables
-    fetch(url, {
+    let response = await fetch(url, {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json' // Specify the content type as JSON
+            'Content-Type': 'application/json' // Specify the content type as JSON
         },
         body: JSON.stringify({scene: scene, sounds: chosen_sounds, length: 100}) // Convert the dataObject to a JSON string
-      })
-        .then(response => {
-          // Handle the response
-          if (response.ok) {
-            return response.json(); // Parse the JSON response if needed
-          } else {
-            throw new Error('Network response was not ok.');
-          }
-        })
-        .then(data => {
-          // Handle the data returned from the server
-          console.log(data);
-        })
-        .catch(error => {
-          // Handle errors
-          console.error('There was a problem with the fetch operation:', error);
-        });
+    })
+    let text = "";
+    // Handle the response
+    if (response.ok) {
+        text = await response.json(); // Parse the JSON response if needed
+    } else {
+        throw new Error('Network response was not ok.');
+    }
+    let utterance = new SpeechSynthesisUtterance("");
+    utterance.text = text;
+    utterance.rate *= 0.60;
+    console.log(utterance.rate);
+    window.speechSynthesis.speak(utterance);
+
     //wait 2 minutes to TTS the text
 
     //ask user for how they felt after the session
