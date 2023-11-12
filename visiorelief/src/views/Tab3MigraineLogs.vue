20342094a3a8a48152b7f997b1ba7c719cf99bfc
Max van Ling <q.m.v.ling@student.tue.nl>
2023-11-12 04:39:59 +0200

    Renamed Migraines to Sessions

--- visiorelief/src/views/Tab3MigraineLogs.vue
+++ visiorelief/src/views/Tab3MigraineLogs.vue
@@ -2,13 +2,13 @@
   <ion-page>
     <ion-header  class="ion-no-border"> 
       <ion-toolbar>
-        <ion-title style="color: #415D7A; text-align: center" >Migraine history</ion-title>
+        <ion-title style="color: #415D7A; text-align: center" >Session history</ion-title>
       </ion-toolbar>
     </ion-header>
     <ion-content :fullscreen="true">
       <ion-header collapse="condense">
         <ion-toolbar>
-          <ion-title size="large">Migraine history</ion-title>
+          <ion-title size="large">Session history</ion-title>
         </ion-toolbar>
       </ion-header>
 
